7a65a038046a829225fb45704afa06f62ddaafa7
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-05-09 21:01:20 +0900

    removed example under main project

--- abukuma-example/src/main/java/rip/deadcode/abukuma3/example/Main.java
+++ abukuma-example/src/main/java/rip/deadcode/abukuma3/example/Main.java
@@ -1,9 +1,12 @@
 package rip.deadcode.abukuma3.example;
 
 import rip.deadcode.abukuma3.Abukuma;
+import rip.deadcode.abukuma3.parser.UrlEncoded;
 import rip.deadcode.abukuma3.router.AbuRouters;
 import rip.deadcode.abukuma3.value.AbuConfigs;
 import rip.deadcode.abukuma3.value.AbuResponses;
+
+import java.nio.file.Paths;
 
 
 public final class Main {
@@ -14,6 +17,19 @@ public final class Main {
                .router( AbuRouters.builder()
                                   .get( "/", req -> AbuResponses.create( "<h1>hello, world</h1>" )
                                                                 .header( h -> h.contentType( "text/html" ) ) )
+                                  .get( "/user/:name", req -> AbuResponses.create(
+                                          String.format( "<h1>hello, %s!</h1>", req.pathParams().get( "name" ) ) )
+                                                                          .header( h -> h.contentType( "text/html" ) )
+                                  )
+                                  .resource( "/resource", "rip/deadcode/abukuma3/example/post.html" )
+                                  .resources( "/resources", "rip/deadcode/abukuma3/example" )
+                                  .file( "/file", Paths.get( "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example/index.html" ) )
+                                  .dir( "/dir", Paths.get( "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example" ) )
+                                  .post( "/post", req -> AbuResponses.create(
+                                          String.format(
+                                                  "<h1>hello, %s!</h1>",
+                                                  req.body( UrlEncoded.class ).getValue( "name" )
+                                          ) ) )
                                   .notFound( req -> AbuResponses.create( "<h1>not found</h1>" )
                                                                 .header( h -> h.contentType( "text/html" ) ) )
                                   .build() )
