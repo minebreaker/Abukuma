9b7d5770c38ca9a6747825e0d8869d44eb789648
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2019-05-20 21:05:25 +0900

    map

--- abukuma/src/test/java/rip/deadcode/abukuma3/collection/AbuPersistentMapTest.java
+++ abukuma/src/test/java/rip/deadcode/abukuma3/collection/AbuPersistentMapTest.java
@@ -3,17 +3,33 @@ package rip.deadcode.abukuma3.collection;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth8.assertThat;
 
 
 class AbuPersistentMapTest {
+
     @Test
-    void test() {
+    void testMayGet() {
+        assertThat( AbuPersistentMap.create() ).isEmpty();
 
-        AbuPersistentMap<String, String> map = AbuPersistentMap.create();
-        assertThat( map ).isEmpty();
+        assertThat( AbuPersistentMap.create().set( "k", "v" ).mayGet( "k" ) ).hasValue( "v" );
+        assertThat( AbuPersistentMap.create().set( "k", "v" ).mayGet( "nothing" ) ).isEmpty();
+    }
 
-        map = map.set( "k1", "v1" ).set( "k2", "v2" );
-        assertThat( map ).containsExactly( "k1", "v1", "k2", "v2" );
-        assertThat( map.delete( "k2" ) ).doesNotContainKey( "k2" );
+    @Test
+    void testSet() {
+
+        AbuPersistentMap<String, String> param = AbuPersistentMap.<String, String>create()
+                .set( "k1", "v1" ).set( "k2", "v2" );
+        assertThat( param ).containsExactly( "k1", "v1", "k2", "v2" );
+        assertThat( param.set( "k2", "replaced" ) ).containsExactly( "k1", "v1", "k2", "replaced" );
+    }
+
+    @Test
+    void testDelete() {
+
+        AbuPersistentMap<String, String> param = AbuPersistentMap.<String, String>create()
+                .set( "k1", "v1" ).set( "k2", "v2" );
+        assertThat( param.delete( "k2" ) ).containsExactly( "k1", "v1" );
     }
 }
