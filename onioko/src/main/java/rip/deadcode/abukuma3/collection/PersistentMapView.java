2f9bf02578e7bac44943fe6af542ab6dafba630c
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2020-07-18 16:27:59 +0900

    PersistentMapView

---
+++ onioko/src/main/java/rip/deadcode/abukuma3/collection/PersistentMapView.java
@@ -0,0 +1,13 @@
+package rip.deadcode.abukuma3.collection;
+
+import java.util.Map;
+
+
+public interface PersistentMapView<K, V, T extends PersistentMapView<K, V, T>> extends PersistentMap<K, V> {
+
+    @Override public T set( K key, V value );
+
+    @Override public T delete( K key );
+
+    @Override public T merge( Map<K, V> map );
+}
