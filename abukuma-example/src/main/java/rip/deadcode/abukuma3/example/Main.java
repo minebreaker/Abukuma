342e03b6487e566f3bb1f46fed234c4c627cf098
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2020-07-21 13:31:16 +0900

    reformat

--- abukuma-example/src/main/java/rip/deadcode/abukuma3/example/Main.java
+++ abukuma-example/src/main/java/rip/deadcode/abukuma3/example/Main.java
@@ -18,22 +18,22 @@ public final class Main {
                                .get( "/", ( ctx, req ) -> Responses.create( "<h1>hello, world</h1>" )
                                                                    .header( h -> h.contentType( "text/html" ) ) )
                                .get( "/user/:name", ( ctx, req ) -> Responses.create(
-                                          String.format( "<h1>hello, %s!</h1>", req.pathParams().get( "name" ) ) )
+                                       String.format( "<h1>hello, %s!</h1>", req.pathParams().get( "name" ) ) )
                                                                              .header( h -> h.contentType(
-                                                                                           "text/html" ) ) )
+                                                                                     "text/html" ) ) )
                                .post( "/post", ( ctx, req ) -> Responses.create(
-                                          String.format(
-                                                  "<h1>hello, %s!</h1>",
-                                                  req.body( UrlEncoded.class ).getValue( "name" )
-                                          ) ) )
+                                       String.format(
+                                               "<h1>hello, %s!</h1>",
+                                               req.body( UrlEncoded.class ).getValue( "name" )
+                                       ) ) )
                                .context( "context", r -> r
-                                          .path( "GET", "/nest", ( ctx, req ) ->
-                                                  Responses.create( "<h1>context</h1>" )
-                                                           .header( h -> h.contentType( "text/html" ) ) ) )
+                                       .path( "GET", "/nest", ( ctx, req ) ->
+                                               Responses.create( "<h1>context</h1>" )
+                                                        .header( h -> h.contentType( "text/html" ) ) ) )
                                .file( "/file", Paths.get(
-                                          "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example/index.html" ) )
+                                       "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example/index.html" ) )
                                .dir( "/dir", Paths.get(
-                                          "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example" ) )
+                                       "./abukuma-example/src/main/resources/rip/deadcode/abukuma3/example" ) )
                                .resource( "/resource", "rip/deadcode/abukuma3/example/index.html" )
                                .resources( "/resources", "rip/deadcode/abukuma3/example" )
                                .notFound( ( ctx, req ) -> Responses.create( "<h1>not found</h1>" )
