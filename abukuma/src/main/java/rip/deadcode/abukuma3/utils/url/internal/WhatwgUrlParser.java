44185f6f17589642a2180ce298b47f4248f8cda7
minebreaker <takutokoyama.terraflopsjapan@gmail.com>
2022-01-18 22:41:51 +0900

    wip

---
+++ abukuma/src/main/java/rip/deadcode/abukuma3/utils/url/internal/WhatwgUrlParser.java
@@ -0,0 +1,10 @@
+package rip.deadcode.abukuma3.utils.url.internal;
+
+
+/**
+ * URL parser that is (almost) compatible with
+ * <a href="https://url.spec.whatwg.org/#concept-basic-url-parser">WHATWG basic URL parser</a>.
+ */
+public final class WhatwgUrlParser {
+
+}
---
+++ documentation/coding_rules.md
@@ -0,0 +1,12 @@
+# Coding rules for Abukuma itself (not for the application using it)
+
+
+## APIs
+
+* Receive `List`, return `PersistentList`
+
+## Namings
+
+* Prefer long name.
+  * Better than not-understandable
+  * IDE will support it.
